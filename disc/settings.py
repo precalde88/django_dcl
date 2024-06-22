51317ebc1a8abe3fac7f75b542d471600dbeafc5
precalde88 <pablo.recalde@exxis-group.com>
2024-06-22 13:58:45 -0400

    Fix postgres for railway

--- disc/settings.py
+++ disc/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'disc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RokaqsNAZbqZNxNxysEtQKVHKFqwQCCf',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '26720',
     }
 }
 
