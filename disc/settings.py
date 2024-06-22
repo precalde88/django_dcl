1523098f4a9c36a22b53888bf9564df6d6c78c07
precalde88 <pablo.recalde@exxis-group.com>
2024-06-22 11:46:09 -0400

    Ajustes para railway

--- disc/settings.py
+++ disc/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/plantillas/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "base/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'plantillas')
 
